0c49844adb6c9af236e19fdeb09fbc907ff57d7d
rensl <rensl@glodon.com>
2019-10-29 08:56:05 +0800

    重构代码

---
+++ WRV Server/database-utils.js
@@ -0,0 +1,168 @@
+var sqlite3 = require('sqlite3');
+var fs = require('fs');
+var path = require('path')
+
+var database = './serverDB/serverTest.db'
+
+var max_item_id = -1
+
+var cell_formats_table = 'CellFormats'
+var row_content_table = 'RowContent'
+var cell_content_table = 'CellContent'
+var grp_files_table = 'GRPFiles'
+
+var data_base_table = [grp_files_table, cell_formats_table, row_content_table, cell_content_table]
+
+var db = new sqlite3.Database(path.join(__dirname, database), function (err) {
+  console.log(err)
+  data_base_table.forEach(element => {
+    db.all('select max(ItemID) from ' + element, function (err, res) {
+      if (!err) {
+        res.forEach(element => {
+          var id = +element['max(ItemID)'];
+          if (id > max_item_id) {
+            max_item_id = id
+          }
+        });
+      }
+    });
+  })
+});
+
+
+function getItemID() {
+  max_item_id += 1;
+  return max_item_id;
+}
+
+
+function insertCellFormatsRecord(data, fileID) {
+  data.forEach(CellFormats => {
+    var sql = `insert into ${cell_formats_table}(ItemID, FileID, FontIsBold, BottomMargin, LeftLineWidth, RightLineWidth, FontName, FontSize, BottomLineWidth,\
+      TopLineWidth, TopMargin, VertAlignment, FontColor, RightMargin, HorzAlignment, FontIsUnderLine, FontIsItalic, LeftMargin)\
+      VALUES(${getItemID()}, ${fileID}, ${CellFormats.FontIsBold}, ${CellFormats.BottomMargin}, ${CellFormats.LeftLineWidth}, ${CellFormats.RightLineWidth}, ${CellFormats.FontName},
+      ${CellFormats.FontSize}, ${CellFormats.BottomLineWidth}, ${CellFormats.TopLineWidth}, ${CellFormats.TopMargin}, ${CellFormats.VertAlignment}, ${CellFormats.FontColor},
+      ${CellFormats.RightMargin}, ${CellFormats.HorzAlignment}, ${CellFormats.FontIsUnderLine}, ${CellFormats.FontIsItalic}, ${CellFormats.LeftMargin})`
+
+    sql.replace('false', 0)
+    sql.replace('true', 1)
+
+    db.run(sql)
+  })
+}
+
+function insertGRPFilesRecord(data, callback) {
+  var itemID = getItemID().toString();
+  var fileName = data.fileName
+  var adjustTime = data.adjustTime
+
+  var sql = `insert into ${grp_files_table}(ItemID,fileName,adjustTime) VALUES (${itemID}, '${fileName}', '${adjustTime}')`
+
+  db.run(sql)
+
+  callback(itemID)
+
+  return itemID;
+}
+
+function insertCellContentRecord(data, fileID, RowID) {
+  itemID = getItemID().toString()
+
+  var sql = `insert into ${cell_content_table}(ItemID, FileID, Row, Col, CellFormat, Value, ColSpan, DataType, RowSpan)
+  VALUES(${itemID}, ${fileID}, ${RowID}, ${data.Col}, ${data.CellFormat}, '${data.Value}', ${data.ColSpan}, ${data.DataType}, ${data.RowSpan})`
+
+  sql.replace('false', 0)
+  sql.replace('true', 1)
+
+  db.run(sql)
+}
+
+function insertRowContentRecord(data, fileID) {
+  data.forEach(rowData => {
+    itemID = getItemID().toString()
+    var sql = `insert into ${row_content_table}(ItemID, FileID, Row) VALUES(${itemID}, ${fileID}, ${rowData.RowID.toString()})`
+
+    db.run(sql)
+
+    var cells = rowData.RowData;
+    cells.forEach(cellData => {
+      insertCellContentRecord(cellData, fileID, itemID)
+    });
+  });
+}
+
+var getAllfiles = function (callback) {
+  var data = ''
+  db.all(`select * from ${grp_files_table}`, function (err, res) {
+    var array = new Array()
+    if (!err) {
+      res.forEach(element => {
+        array.push({
+          index: element.ItemID,
+          name: element.FileName,
+          time: element.AdjustTime
+        })
+      });
+      data = JSON.stringify(array);
+      callback(data)
+    }
+  });
+}
+
+exports.getAllfiles = getAllfiles
+
+exports.deletefile = function (index, callback) {
+  db.all(`select * from ${grp_files_table} where ItemID = ${index}`, function (err, res) {
+    var sql1 = `DELETE FROM ${cell_content_table} WHERE FileID = ${index}`
+    var sql2 = `DELETE FROM ${cell_formats_table} WHERE FileID = ${index}`
+    var sql3 = `DELETE FROM ${row_content_table} WHERE FileID = ${index}`
+    var sql4 = `DELETE FROM ${grp_files_table} WHERE ItemID = ${index}`
+
+    db.run(sql1)
+    db.run(sql2)
+    db.run(sql3)
+    db.run(sql4)
+
+    getAllfiles(function (data) {
+      callback(data)
+    })
+  })
+}
+
+exports.uploadFiles = function (post) {
+  var fileID = '';
+
+  if (post.File) {
+    fileID = insertGRPFilesRecord(post.File, function (id) {
+      var data = {
+        CellFormats: post.CellFormats,
+        PageContent: post.PageContent
+      }
+
+      fs.writeFile(`./JsonData/${id}.json`, JSON.stringify(data), function () {})
+    })
+  } else {
+    return
+  }
+
+  for (var element in post) {
+    if ('CellFormats' === element) {
+      insertCellFormatsRecord(post.CellFormats, fileID)
+    } else if ('PageContent' === element) {
+      insertRowContentRecord(post.PageContent, fileID)
+    }
+  }
+}
+
+exports.getFileContent = function (id, callback) {
+  fs.exists(`./JsonData/${id}.json`, function (exists) {
+    if (exists) {
+      var data = fs.readFileSync('./JsonData/' + id.toString() + '.json');
+      callback(data.toString())
+    } else {
+      console.log('File not exists.')
+    }
+  })
+}
+
+// db.close()
---
+++ WRV Server/server.js
@@ -0,0 +1,71 @@
+const http = require('http');
+const url = require('url');
+const qs = require('querystring');
+
+const database = require('./database-utils');
+
+const defaultHostName = 'http://localhost:8080'
+
+// create server
+const server = http.createServer((req, res) => {
+   res.setHeader("Access-Control-Allow-Origin", defaultHostName);
+   res.setHeader("Access-Control-Allow-Credentials", "true");
+   res.setHeader("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+   res.setHeader("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+});
+
+// listen client request
+server.on('request', (req, res) => {
+   let urlObject = url.parse(req.url, true);
+   let pathname = urlObject.pathname
+
+   // deal with get
+   if (pathname == "/fileList" || pathname == "/") {
+      database.getAllfiles(function (data) {
+         res.end(data)
+      })
+   } else if (pathname == "/delete") {
+      let index = urlObject.query.index;
+      database.deletefile(index, function (data) {
+         res.end(data)
+      })
+   } else if (pathname == "/getdata") {
+      let index = urlObject.query.index;
+      database.getFileContent(index, function (data) {
+         res.end(data)
+      })
+   };
+
+   // deal with post
+   let post = ''
+   req.on('data', chunk => {
+      post += chunk;
+   });
+
+   req.on('end', function () {
+      post = qs.parse(post);
+      for (let element in post) {
+         if (req.url == "/upload") {
+            let dataJson = JSON.parse(element)
+            database.uploadFiles(dataJson);
+         } else if (req.url == "modify") {
+
+         }
+      }
+   });
+});
+
+// server on connect
+server.on('connect', req => {
+   const {
+      port,
+      hostname
+   } = new URL(`http://${req.url}`);
+
+   console.log(`${hostname}:${port} is connect.`);
+});
+
+// server listen on 9090
+server.listen(9090, '127.0.0.1', () => {
+   console.log('Server start on http://localhost:9090.');
+});
